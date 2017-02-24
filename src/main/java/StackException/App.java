9251e0ce19a83c37cd2812fe262ca9a94a560121
munozloisivan <munozloisivan@gmail.com>
2017-02-24 09:01:52 +0100

    Log4j incorporado con diferentes niveles de log INFO, WARN y ERROR

--- src/main/java/StackException/App.java
+++ src/main/java/StackException/App.java
@@ -1,38 +1,46 @@
 package StackException;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import static org.apache.log4j.Logger.*;
+
 public class App
 {
     public static void main( String[] args ){
+
+        BasicConfigurator.configure();
+        final Logger logPush = getLogger("Push");
+        final Logger logPop = getLogger("Pop");
+
         StackImplement implementacion = new StackImplement();
 
         try {
+            logPush.info("Procedemos a llenar la pila \n");
             implementacion.push(1);
             implementacion.push(2);
+            logPush.info("Tamaño actual: " +implementacion.size() +"\n");
             implementacion.push(3);
             implementacion.push(10);
-            System.out.println("tamaño antes de reventarla: "+implementacion.size());
-            System.out.println("Intento añadir uno mas aver si salta excepcion");
+            logPush.info("Tamaño actual: " +implementacion.size()+"\n");
+            logPush.warn("Tamaño máximo alcanzado\n");
             implementacion.push(15);
-            System.out.println("tamaño" + implementacion.size());
         }
         catch (FullStackException e){
-            System.out.println(e.getMessage());
+           logPush.error(e.getMessage()+"\n");
         }
-
 
         try {
-            System.out.println("ahora la vacio");
+            logPop.info("Procedemos a vaciar la pila \n");
             implementacion.pop();
             implementacion.pop();
+            logPop.info("Tamaño actual: " +implementacion.size()+"\n");
             implementacion.pop();
             implementacion.pop();
-            System.out.println("tendria que saltar ahora la excepcion");
+            logPop.warn("El tamaño actual es: " +implementacion.size()+" NO se puede quitar nada mas \n");
             implementacion.pop();
         } catch (EmptyStackException e) {
-            System.out.println(e.getMessage());
+            logPop.error(e.getMessage()+"\n");
         }
-
-        System.out.println("tamaño" + implementacion.size());
-
     }
 }
